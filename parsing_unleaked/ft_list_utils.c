35cbe441ba8aaed3db5a6c32e38c055f9927e09b
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-12-21 16:02:36 +0100

    dollars and redirections missing

---
+++ parsing_unleaked/ft_list_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/14 16:57:41 by rvalton           #+#    #+#             */
+/*   Updated: 2021/12/19 15:53:48 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+/*return the number of element of a list of type t_cmd*/
+
+int	ft_size_list_cmd(t_cmd *list)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	if (list == NULL)
+		return (0);
+	tmp = list;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->nxt;
+		i++;
+	}
+	return (i);
+}
+
+/*print in stdout what's contain a list of type t_cmd*/
+
+void	ft_print_list_cmd(t_cmd *list)
+{
+	t_cmd	*tmp;
+	int		i;
+
+	if (list != NULL)
+	{
+		tmp = list;
+		while (tmp)
+		{
+			i = -1;
+			while (tmp->argv[++i])
+			{
+				ft_print_str(tmp->argv[i]);
+				if (tmp->argv[i + 1])
+					ft_print_str("//");
+			}
+			printf(" is_pipe=%d\n", tmp->is_cmd_after_pipe);
+			tmp = tmp->nxt;
+		}
+	}
+}
---
+++ parsing_unleaked/ft_prompt.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_prompt.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 12:33:23 by rvalton           #+#    #+#             */
+/*   Updated: 2021/12/21 15:32:58 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+static void	ft_init_pointers(t_cmd **cmd, char **line)
+{
+	*cmd = NULL;
+	*line = NULL;
+}
+
+static int	ft_is_eof(char *line)
+{
+	if (!line)
+	{
+		free(line);
+		return (1);
+	}
+	return (0);
+}
+
+static void	ft_parse_readline(t_cmd **cmd, char **line)
+{
+	char	**cmd_char;
+	char	**pipe_char;
+	int		i;
+
+	cmd_char = NULL;
+	pipe_char = NULL;
+	pipe_char = ft_pipe_split(line);
+	i = -1;
+	while (pipe_char[++i])
+	{
+		cmd_char = ft_cmdline_split(&pipe_char[i]);
+		cmd_char = ft_clean_cmdchar(cmd_char);
+		*cmd = ft_eadd_tolist_cmd(*cmd, cmd_char, i);
+	}
+	free(pipe_char);
+}
+
+int	ft_prompt(void)
+{
+	t_cmd	*cmd;
+	char	*line;
+
+	ft_init_pointers(&cmd, &line);
+	line = readline("$>");
+	if (ft_is_eof(line))
+		return (1);
+	if (ft_strlen(line) > 0)
+	{
+		add_history(line);
+		if (!ft_parse_error(line))
+		{
+			free(line);
+			printf("syntax error\n");
+			return (0);
+		}
+		ft_parse_readline(&cmd, &line);
+	}
+	if (!cmd)
+		free(line);
+	else
+		ft_free_list_cmd(&cmd);
+	return (0);
+}
