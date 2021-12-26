eb77de0bc181dbaa502731cdb46e99d3ce572cac
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-12-26 05:41:10 +0100

    only redir are missing

---
+++ redir_missing/ft_builtin.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_builtin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 12:33:23 by rvalton           #+#    #+#             */
+/*   Updated: 2021/12/26 04:21:48 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+int	ft_is_builtin(char *exe)
+{
+	if (ft_strcomp("env", exe) == 0)
+		return (1);
+	else if (ft_strcomp("cd", exe) == 0)
+		return (1);
+	else if (ft_strcomp("unset", exe) == 0)
+		return (1);
+	else if (ft_strcomp("export", exe) == 0)
+		return (1);
+	else if (ft_strcomp("pwd", exe) == 0)
+		return (1);
+	else if (ft_strcomp("echo", exe) == 0)
+		return (1);
+	return (0);
+}
+
+int	ft_exec_builtin(t_env **env, char **argv)
+{
+	if (ft_strcomp("env", argv[0]) == 0)
+		return (ft_env(*env, argv));
+	else if (ft_strcomp("cd", argv[0]) == 0)
+		return (ft_hdl_arg_cd(env, argv));
+	else if (ft_strcomp("unset", argv[0]) == 0)
+		return (ft_hdl_arg_unset(env, argv));
+	else if (ft_strcomp("export", argv[0]) == 0)
+		return (ft_hdl_arg_export(env, argv));
+	else if (ft_strcomp("pwd", argv[0]) == 0)
+		return (ft_pwd());
+	else if (ft_strcomp("echo", argv[0]) == 0)
+		return (ft_echo(argv));
+	else
+		return (2);
+}
---
+++ redir_missing/ft_exit.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 09:15:23 by rvalton           #+#    #+#             */
+/*   Updated: 2021/12/26 03:27:42 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+
+int	ft_wrong_exit_arg(char *code)
+{
+	int	i;
+
+	i = 0;
+	while (ft_is_digit(code[i]))
+		i++;
+	if (i > 3 || code[i] != '\0' || ft_atoi(code) > 255)
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_exit(t_env **env, t_cmd **cmd, char **line, char *code)
+{
+	write(1, "exit\n", 5);
+	if (!code)
+		;
+	else if (ft_wrong_exit_arg(code))
+	{
+		g_exit_status = 2;
+		write(2, "exit: ", 6);
+		write(2, code, ft_strlen(code));
+		write(2, ": numeric argument required\n", 28);
+	}
+	else
+		g_exit_status = ft_atoi(code);
+	free(*line);
+	ft_free_list_cmd(cmd);
+	ft_free_list_env(env);
+	exit(g_exit_status);
+	return (g_exit_status);
+}
